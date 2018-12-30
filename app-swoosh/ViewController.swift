95d4ca5e00133b835a1ab8dac11876eb0a9af151
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-12-30 18:49:58 -0500

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Ricardo Herrera Petit on 12/30/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

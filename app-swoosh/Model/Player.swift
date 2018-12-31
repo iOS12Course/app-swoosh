2dbcfac29c7633d23064371d63875ad1bee97673
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-12-31 12:30:13 -0500

    Created model and implemented IBActions

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ricardo Herrera Petit on 12/31/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague:String?
+    var selectedSkillLevel: String?
+    
+}
